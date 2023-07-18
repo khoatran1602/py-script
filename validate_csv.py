7e4e5a390a2508c8106a804ce3efe0802275510b
Tran Dang Khoa <trankhoa758@gmail.com>
2023-07-18 14:04:23 +0700

    fix

--- validate_csv.py
+++ validate_csv.py
@@ -5,10 +5,10 @@ csv_file_path = sys.argv[1]
 
 try:
     with open(csv_file_path, 'r') as file:
-        dialect = csv.Sniffer().sniff(file.read())
-        file.seek(0)
-        print("true")
+        dialect = csv.Sniffer().sniff(file.read(1024))
+        if dialect.delimiter == ',':
+            print("true")
+        else:
+            print("false")
 except csv.Error:
     print("Invalid file format. Not a valid CSV file.")
-
-
